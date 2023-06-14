d4ef4653547e157b2f1381ee56731202fa53a65d
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2023-06-14 17:59:03 +0200

    fix EnrollmentsDAOImpl

--- src/main/java/org/consoleApp/dao/jdbc/EnrollmentsDAOImpl.java
+++ src/main/java/org/consoleApp/dao/jdbc/EnrollmentsDAOImpl.java
@@ -12,30 +12,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EnrollmentsDAOImpl implements EnrollmentsDAO {
-    private Connection connection;
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
+    private DataSource dataSource;
 
     public EnrollmentsDAOImpl(DataSource dataSource) {
-        try {
-            this.connection = dataSource.getConnection();
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to establish a database connection", e);
-        }
+        this.dataSource = dataSource;
     }
 
     @Override
     public boolean deleteStudentById(int studentId) {
-        try {
-            preparedStatement = connection.prepareStatement("DELETE FROM enrollments WHERE student_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM enrollments WHERE student_id = ?")){
             preparedStatement.setInt(1, studentId);
 
             int rowsAffected = preparedStatement.executeUpdate();
             return rowsAffected > 0;
         } catch (SQLException e) {
             throw new IllegalStateException("Can't delete student by id", e);
-        } finally {
-            closeResources();
         }
     }
 
@@ -43,20 +35,19 @@ public class EnrollmentsDAOImpl implements EnrollmentsDAO {
     public List<Integer> findAllStudentsIdByCourseId(int courseId) {
         List<Integer> studentsId = new ArrayList<>();
 
-        try {
-            preparedStatement = connection.prepareStatement("SELECT student_id FROM enrollments WHERE course_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT student_id FROM enrollments WHERE course_id = ?")){
             preparedStatement.setInt(1, courseId);
-            resultSet = preparedStatement.executeQuery();
 
+        try (ResultSet resultSet = preparedStatement.executeQuery()){
             while (resultSet.next()){
                 int currentId = resultSet.getInt("student_id");
                 studentsId.add(currentId);
             }
+        }
 
         } catch (SQLException e) {
             throw new IllegalStateException("Can't find students id by course id", e);
-        } finally {
-            closeResources();
         }
 
         return studentsId;
@@ -65,20 +56,19 @@ public class EnrollmentsDAOImpl implements EnrollmentsDAO {
     public List<Integer> findAllCourseIdByStudentsId(int studentId) {
         List<Integer> coursesId = new ArrayList<>();
 
-        try {
-            preparedStatement = connection.prepareStatement("SELECT course_id FROM enrollments WHERE student_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT course_id FROM enrollments WHERE student_id = ?")){
             preparedStatement.setInt(1, studentId);
-            resultSet = preparedStatement.executeQuery();
 
+        try (ResultSet resultSet = preparedStatement.executeQuery()){
             while (resultSet.next()){
                 int currentId = resultSet.getInt("course_id");
                 coursesId.add(currentId);
             }
+        }
 
         } catch (SQLException e) {
             throw new IllegalStateException("Can't find course id by student id", e);
-        } finally {
-            closeResources();
         }
 
         return coursesId;
@@ -86,8 +76,8 @@ public class EnrollmentsDAOImpl implements EnrollmentsDAO {
 
     @Override
     public boolean removeStudentFromCourse(int studentId, int courseId) {
-        try {
-            preparedStatement = connection.prepareStatement("DELETE FROM enrollments WHERE student_id = ? AND course_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM enrollments WHERE student_id = ? AND course_id = ?")){
             preparedStatement.setInt(1, studentId);
             preparedStatement.setInt(2, courseId);
 
@@ -95,25 +85,6 @@ public class EnrollmentsDAOImpl implements EnrollmentsDAO {
             return rowsAffected > 0;
         } catch (SQLException e) {
             throw new IllegalStateException("Can't remove student from course", e);
-        } finally {
-            closeResources();
-        }
-    }
-    private void closeResources(){
-        if (resultSet != null) {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                throw new RuntimeException("Something wrong with ResultSet in EnrollmentsDAOImpl", e);
-            }
-        }
-
-        if (preparedStatement != null){
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                throw new RuntimeException("Something wrong with PreparedStatement in EnrollmentsDAOImpl", e);
-            }
         }
     }
 }

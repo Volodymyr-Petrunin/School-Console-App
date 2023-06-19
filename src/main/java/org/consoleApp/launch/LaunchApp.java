0e0c50365eb5e2ec46d6c4dcfd0df8e99a8222ca
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2023-06-19 16:50:07 +0200

    fix tree

--- src/main/java/org/consoleApp/launch/LaunchApp.java
+++ src/main/java/org/consoleApp/launch/LaunchApp.java
@@ -13,14 +13,11 @@ import org.consoleApp.dataFilling.impl.StudentsDataFiller;
 import org.consoleApp.dao.jdbc.GroupsDAOImpl;
 import org.consoleApp.dao.jdbc.StudentsDAOImpl;
 import org.consoleApp.generation.records.InitialAmountGeneration;
-import org.consoleApp.menu.leaf.*;
-import org.consoleApp.menu.MenuItem;
 import org.consoleApp.menu.composite.MenuOption;
 import org.consoleApp.parser.impl.CourseParser;
 import org.consoleApp.readers.ResourcesFileReader;
 
 import javax.sql.DataSource;
-import java.util.*;
 
 public class LaunchApp {
     private final DBConnector dbConnector = new DBConnector();
@@ -41,42 +38,14 @@ public class LaunchApp {
     private final StudentsDataFiller studentsDataFiller = new StudentsDataFiller(readerFirstName.read(), readerSecondName.read(), amountGeneration, studentsDAO, groupsDAO);
     private final CoursesDataFiller coursesDataFiller = new CoursesDataFiller(courseParser.parsedList(readerCourses.read()), courseDAO);
     private final EnrollmentsDataFiller enrollmentsDataFiller = new EnrollmentsDataFiller(3, studentsDAO, courseDAO);
-    private final Scanner scan = new Scanner(System.in);
     private final String dash = "-".repeat(50); // yes magic number. But it doesn't affect anything
-    private final List<MenuItem> menuItems = createMenuItems();
-    private final MenuOption menuOption = new MenuOption(menuItems, dash);
     private final DataFillerOption fillerOption = new DataFillerOption(coursesDataFiller, groupDataFiller, studentsDataFiller, enrollmentsDataFiller);
-    private boolean exit = false;
+    private final MenuOption menuOption = new MenuOption(groupsDAO, courseDAO, studentsDAO, enrollmentsDAO, dash);
+
     public void launch(){
         scriptRunner.runScript(scriptCreateTables);
         fillerOption.fillData();
 
-        while (!exit){
-            menuOption.execute();
-            userChooses();
-        }
-    }
-
-    private void userChooses(){
-        System.out.print("Yor choice: ");
-        int currentChoice = scan.nextInt();
-
-        if (currentChoice >= 1 && currentChoice <= menuItems.size()) {
-            menuItems.get(currentChoice - 1).execute();
-        } else if (currentChoice == 0) {
-            exit = true;
-        }
-    }
-
-    private List<MenuItem> createMenuItems(){
-        List<MenuItem> menu = new ArrayList<>();
-        menu.add(new MenuFindGroupsWithLessOrEqualStudents(groupsDAO, dash));
-        menu.add(new MenuFindAllStudentsRelatedToCourse(courseDAO, enrollmentsDAO, studentsDAO, groupsDAO, dash));
-        menu.add(new MenuAddNewStudent(groupsDAO, studentsDAO, dash));
-        menu.add(new MenuDeleteStudent(enrollmentsDAO, studentsDAO, dash));
-        menu.add(new MenuAddStudentToCourse(studentsDAO, courseDAO, groupsDAO, dash));
-        menu.add(new MenuRemoveStudentFromOneOfCourses(studentsDAO, enrollmentsDAO, courseDAO, groupsDAO, dash));
-
-        return menu;
+        menuOption.execute();
     }
 }
--- src/main/java/org/consoleApp/menu/composite/MenuOption.java
+++ src/main/java/org/consoleApp/menu/composite/MenuOption.java
@@ -1,17 +1,35 @@
 package org.consoleApp.menu.composite;
 
+import org.consoleApp.dao.CourseDAO;
+import org.consoleApp.dao.GroupDAO;
+import org.consoleApp.dao.StudentsDAO;
+import org.consoleApp.dao.jdbc.EnrollmentsDAO;
 import org.consoleApp.menu.MenuItem;
+import org.consoleApp.menu.leaf.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class MenuOption implements MenuItem {
+    private final Scanner scan = new Scanner(System.in);
     private List<MenuItem> menuItems;
+    private boolean exit;
+    private GroupDAO groupsDAO;
+    private CourseDAO courseDAO;
+    private StudentsDAO studentsDAO;
+    private EnrollmentsDAO enrollmentsDAO;
     private String dash;
 
-    public MenuOption(List<MenuItem> menuItems, String dash) {
-        this.menuItems = menuItems;
+    public MenuOption(GroupDAO groupsDAO, CourseDAO courseDAO, StudentsDAO studentsDAO, EnrollmentsDAO enrollmentsDAO, String dash) {
+        this.groupsDAO = groupsDAO;
+        this.courseDAO = courseDAO;
+        this.studentsDAO = studentsDAO;
+        this.enrollmentsDAO = enrollmentsDAO;
         this.dash = dash;
+        this.exit = false;
+        this.menuItems = createMenuItems();
     }
 
     @Override
@@ -21,21 +39,49 @@ public class MenuOption implements MenuItem {
 
     @Override
     public void execute() {
+        while (!exit){
+            displayMenu();
+            userChooses();
+        }
+    }
+
+    private void displayMenu() {
         StringJoiner menu = new StringJoiner(System.lineSeparator());
 
         menu.add(getDescription());
 
         int place = 1;
-        for (MenuItem item : menuItems){
+        for (MenuItem item : menuItems) {
             menu.add(place++ + ". " + item.getDescription());
         }
 
         menu.add("");
-
         menu.add("0. Exit");
         menu.add(dash);
 
         System.out.println(menu);
     }
 
+    private void userChooses(){
+        System.out.print("Yor choice: ");
+        int currentChoice = scan.nextInt();
+
+        if (currentChoice >= 1 && currentChoice <= menuItems.size()) {
+            menuItems.get(currentChoice - 1).execute();
+        } else if (currentChoice == 0) {
+            exit = true;
+        }
+    }
+
+    private List<MenuItem> createMenuItems(){
+        List<MenuItem> menu = new ArrayList<>();
+        menu.add(new MenuFindGroupsWithLessOrEqualStudents(groupsDAO, dash));
+        menu.add(new MenuFindAllStudentsRelatedToCourse(courseDAO, enrollmentsDAO, studentsDAO, groupsDAO, dash));
+        menu.add(new MenuAddNewStudent(groupsDAO, studentsDAO, dash));
+        menu.add(new MenuDeleteStudent(enrollmentsDAO, studentsDAO, dash));
+        menu.add(new MenuAddStudentToCourse(studentsDAO, courseDAO, groupsDAO, dash));
+        menu.add(new MenuRemoveStudentFromOneOfCourses(studentsDAO, enrollmentsDAO, courseDAO, groupsDAO, dash));
+
+        return menu;
+    }
 }

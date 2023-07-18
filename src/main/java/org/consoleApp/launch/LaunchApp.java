fabda2f2c2f21eb3f985e491d316b9ae8ee6e878
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2023-07-18 13:44:02 +0200

    Added functionality for switching between different implementations

--- src/main/java/org/consoleApp/launch/LaunchApp.java
+++ src/main/java/org/consoleApp/launch/LaunchApp.java
@@ -1,5 +1,11 @@
 package org.consoleApp.launch;
 
+import org.consoleApp.dao.CourseDAO;
+import org.consoleApp.dao.GroupDAO;
+import org.consoleApp.dao.StudentsDAO;
+import org.consoleApp.dao.spring_jdbc.CourseRepository;
+import org.consoleApp.dao.spring_jdbc.GroupRepository;
+import org.consoleApp.dao.spring_jdbc.StudentsRepository;
 import org.consoleApp.dataFilling.DataFiller;
 import org.consoleApp.dataFilling.composite.DataFillerComposite;
 import org.consoleApp.generation.records.GroupAmountGeneration;
@@ -32,8 +38,11 @@ public class LaunchApp {
     private final ResourcesFileReader readerFirstName = new ResourcesFileReader("firstName.txt");
     private final ResourcesFileReader readerSecondName = new ResourcesFileReader("secondName.txt");
     private final CourseDAOImpl courseDAO = new CourseDAOImpl(dataSource);
+    private final CourseRepository courseRepository = new CourseRepository(dataSource);
     private final GroupsDAOImpl groupsDAO = new GroupsDAOImpl(dataSource);
+    private final GroupRepository groupRepository = new GroupRepository(dataSource);
     private final StudentsDAOImpl studentsDAO = new StudentsDAOImpl(dataSource);
+    private final StudentsRepository studentsRepository = new StudentsRepository(dataSource);
     private final CourseParser courseParser = new CourseParser();
     private final GroupAmountGeneration groupAmountGeneration = new GroupAmountGeneration(10, 2, 2);
     private final InitialAmountGeneration amountGeneration = new InitialAmountGeneration(200, 30, 10);
@@ -50,20 +59,27 @@ public class LaunchApp {
                 coursesDataFiller, groupDataFiller, studentsDataFiller, enrollmentsDataFiller
         );
 
-        List<MenuItem> menuItems = List.of(
-                new MenuFindGroupsWithLessOrEqualStudents(groupsDAO, dash),
-                new MenuFindAllStudentsRelatedToCourse(studentsDAO, groupsDAO, dash),
-                new MenuAddNewStudent(groupsDAO, studentsDAO, dash),
-                new MenuDeleteStudent(studentsDAO, dash),
-                new MenuAddStudentToCourse(studentsDAO, courseDAO, groupsDAO, dash),
-                new MenuRemoveStudentFromOneOfCourses(studentsDAO, courseDAO, groupsDAO, dash)
-        );
+        List<MenuItem> menuItems = menuItems(courseDAO, groupsDAO, studentsDAO, dash);
+        List<MenuItem> springMenuItems = menuItems(courseRepository, groupRepository, studentsRepository, dash);
 
         DataFillerComposite fillerComposite = new DataFillerComposite(dataFillers);
-        MenuComposite menuComposite = new MenuComposite(menuItems, dash);
+
+        MenuSwapToSpringJDBC toSpringJDBC = new MenuSwapToSpringJDBC(springMenuItems, dash);
+        MenuComposite menuComposite = new MenuComposite(menuItems, toSpringJDBC, dash);
 
         fillerComposite.fillData();
 
         menuComposite.execute();
+    }
+
+    private List<MenuItem> menuItems(CourseDAO courseDAO, GroupDAO groupDAO, StudentsDAO studentsDAO, String dash){
+        return List.of(
+                new MenuFindGroupsWithLessOrEqualStudents(groupDAO, dash),
+                new MenuFindAllStudentsRelatedToCourse(studentsDAO, groupDAO, dash),
+                new MenuAddNewStudent(groupDAO, studentsDAO, dash),
+                new MenuDeleteStudent(studentsDAO, dash),
+                new MenuAddStudentToCourse(studentsDAO, courseDAO, groupDAO, dash),
+                new MenuRemoveStudentFromOneOfCourses(studentsDAO, courseDAO, groupDAO, dash)
+        );
     }
 }

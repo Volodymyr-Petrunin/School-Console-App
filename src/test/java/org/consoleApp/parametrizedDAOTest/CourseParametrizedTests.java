6c4bbc463bcf55eebf0c726d7b6b47f719b82149
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2023-08-14 16:35:16 +0200

    bug

--- src/test/java/org/consoleApp/parametrizedDAOTest/CourseParametrizedTests.java
+++ src/test/java/org/consoleApp/parametrizedDAOTest/CourseParametrizedTests.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -16,9 +17,11 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest(classes = DaoTestConfig.class)
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles({"spring-jdbc", "native-jdbc"})
+@Sql(scripts = "classpath:SQLScript/create_tables.sql")
 @Sql(scripts = "classpath:SQLScript/course_test_script.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class CourseParametrizedTests {
 
